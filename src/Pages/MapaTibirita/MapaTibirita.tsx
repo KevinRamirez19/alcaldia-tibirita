7fd6111e6ab9b71e45128bd1e0d9de494eb950f7
Kevin200519 <Ksramirez@ucundinamarca.edu.co>
2025-05-17 13:13:35 -0500

    update

--- src/Pages/MapaTibirita/MapaTibirita.tsx
+++ src/Pages/MapaTibirita/MapaTibirita.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { MapContainer, TileLayer, Marker, Popup, LayersControl, LayerGroup } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup, LayersControl } from 'react-leaflet';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import { motion } from 'framer-motion';
